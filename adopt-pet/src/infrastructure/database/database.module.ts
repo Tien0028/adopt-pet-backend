8a841d734c1a84230420445947efa7ec5a27d13b
dpankov91 <dpankov91@gmail.com>
2021-05-10 14:46:43 +0300

    table for Person created

--- adopt-pet/src/infrastructure/database/database.module.ts
+++ adopt-pet/src/infrastructure/database/database.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import PetEntity from '../entities/pet.entity';
+import PersonEntity from "../entities/person.entity";
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import PetEntity from '../entities/pet.entity';
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
-        entities: [PetEntity],
+        entities: [PetEntity, PersonEntity],
         synchronize: true,
       }),
     }),
---
+++ adopt-pet/src/infrastructure/entities/person.entity.ts
@@ -0,0 +1,21 @@
+import {Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn} from "typeorm";
+import PetEntity from "./pet.entity";
+
+@Entity()
+export class PersonEntity {
+    @PrimaryGeneratedColumn()
+    public id: string;
+    @Column()
+    public firstName: string;
+    @Column()
+    public lastName: string;
+    @Column()
+    public email: string;
+    @Column()
+    public phoneNumber: number;
+    @OneToOne(() => PetEntity)
+    @JoinColumn()
+    public pet: PetEntity;
+}
+
+export default PersonEntity;
