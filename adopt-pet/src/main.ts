23bcfe174ad40507f16b63df24723ae37161676c
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2021-05-26 13:03:08 +0200

    Beautify

--- adopt-pet/src/main.ts
+++ adopt-pet/src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService: ConfigService = app.get(ConfigService);
+  // const configService: ConfigService = app.get(ConfigService);
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
-  await app.listen(configService.get('PORT'));
+  // await app.listen(configService.get('PORT'));
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
